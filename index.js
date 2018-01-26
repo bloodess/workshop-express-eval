6eefb37b811691c06b2a29425c68508d933d7634
Bloodess <arnaud.champleboux@gmail.com>
2018-01-26 15:01:47 +0100

    Exercise 1

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ let chalk = require('chalk');
 const app = require('./src/app');
 
 // port
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// start server here
-console.log(chalk.green('Hello web server'));
+// start server
+app.listen(port, function() {
+
+    // start server here
+    console.log(chalk.green('Hello web server'));
+
+});
+
