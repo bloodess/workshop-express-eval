4983161ae15ed039284055020815e0425011e759
Bloodess <arnaud.champleboux@gmail.com>
2018-01-26 16:20:03 +0100

    Exercise 4

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const fs = require('fs');
+//const fileNumber = require('../src/numbers');
 const app = express();
-
+const separator = "\n";
 
 app.get('/bingo', function(req, res) {
     res.status(200);
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify('Bingo'));
+    res.end(JSON.stringify(getFile("src/numbers.txt")));
   });
+
+function getFile(filePath){
+    
+    fs.readFile(filePath, (err, data) => {
+        if (err){
+            throw err;
+        } 
+        else{
+            var stringNumber = data+"";
+            var tabNumber = stringNumber.split(separator).join().slice(0, -1);
+            console.log(tabNumber)
+            return(tabNumber);
+        }
+    });
+}
+//chaine.split(separator);
 
 module.exports = app;
